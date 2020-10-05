072a9d3002e82f0172aecb362a4abf3d1a19b462
Allyy Abdelsallaam <allyyabdelsallaam@gmail.com>
2020-10-05 22:56:24 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import HomePage from './screens/HomePage/HomePage';
+import ShopPage from './screens/shop/Shop';
+import AuthPage from './screens/AuthPage/AuthPage';
+import CheckOut from './screens/checkout/checkout';
+import Header from './components/header/header';
+
+import { auth, createUserProfileDocument } from './firebase/firebase';
+import { setCurrentUser } from './redux/action/users-action';
+import { selectCurrentUser } from './redux/users/user-selector';
 
 function App() {
+
+  const { currentUser } = useSelector(state => ({
+    currentUser: selectCurrentUser(state)
+  }))
+
+  const dispatch = useDispatch();
+
+  var unsubscribeFromAuth = null;
+
+  useEffect(() => {
+    unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          dispatch(setCurrentUser({ id: snapShot.id, ...snapShot.data() }))
+        })
+        //return
+      }
+      dispatch(setCurrentUser(userAuth));
+    })
+
+    return () => {
+      unsubscribeFromAuth();
+    }
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckOut} />
+        <Route exact path='/auth' render={() => currentUser ? <Redirect to='/' /> : <AuthPage />} />
+      </Switch>
     </div>
   );
 }
